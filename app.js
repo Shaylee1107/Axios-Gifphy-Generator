d6971948e2e7d43e1a3469a6785427c0b4981bcb
Shaylee <shayleebaumgartner1107@gmail.com>
2024-02-21 10:39:52 -0500

    gifs are working

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
 console.log("Let's get this party started!");
 
-async function logIn(){
-    const res = await axios.post('http://upload.giphy.com/v1/gifs', { 
-        param : 
-        {api_key: "nXxcwEd9rDgAdXDeVZ3zlITkdvkSOOi5", 
-        username: "ShayleeB", 
-        source_image_url: "http://www.mysite.com/myfile.mp4"
-    }});
-    return res; 
+async function logIn(url){
+    console.log('logIn start')
+    // const res = await axios.post('http://upload.giphy.com/v1/gifs', { 
+    //     param : 
+    //     {api_key: "nXxcwEd9rDgAdXDeVZ3zlITkdvkSOOi5", 
+    //     username: "ShayleeB", 
+    //     source_image_url: url
+    // }});
+    const xhr = $.post("http://upload.giphy.com/v1/gifs", {api_key: "nXxcwEd9rDgAdXDeVZ3zlITkdvkSOOi5", username: "ShayleeB", source_image_url: url});
+    xhr.done(function(data) { console.log("success got data", data); });
+
+    // var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
+    // xhr.done(function(data) { console.log("success got data", data); });
+    // console.log('logIn() res value', res);
+    makeImg(url);
 }
 
-async function getGif(search){
-    logIn();
+async function getUrl(search){
     const res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
-    console.log(res.data);
-    const firstGif = res.data.data[0].url;
-    console.log('this is first gif', firstGif);
-    makeImg(firstGif);
+    console.log('getUrl() res.data', res.data);
+    const firstGif = res.data.data[0].images.downsized.url;
+    // const firstGif = res.data.data[0].url;
+    console.log('getUrl(): this is first gif', firstGif);
+    logIn(firstGif);
 }
 
 function getInput(){
     const input = document.querySelector('#search');
-    console.log(input.value);
-    getGif(input.value);
+    console.log('getInput() this is input value', input.value);
+    getUrl(input.value);
 }
 
 function makeImg(url) {
