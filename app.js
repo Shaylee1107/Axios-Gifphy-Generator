b46e06aafa81138f1577d5216fc5ed0bd3431a10
Shaylee <shayleebaumgartner1107@gmail.com>
2024-02-21 11:39:20 -0500

    styling

--- app.js
+++ app.js
@@ -14,26 +14,28 @@ rmvBtn.addEventListener('click', function(e){
 function getInput(e){
     e.preventDefault();
     const input = document.querySelector('#search');
-    console.log('getInput() this is input value', input.value);
+    console.log('search: ', input.value);
     getUrl(input.value);
     input.value = '';
 }
 
 async function getUrl(search){
     const res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=nXxcwEd9rDgAdXDeVZ3zlITkdvkSOOi5`);
-    console.log('getUrl() res.data', res.data);
-    const firstGif = res.data.data[0].images.downsized.url;
-    console.log('getUrl(): this is first gif', firstGif);
-    makeImg(firstGif);
+    const idxTotal = res.data.data.length;
+    const randomIdx = Math.floor(Math.random() * idxTotal);
+    const gif = res.data.data[randomIdx].images.downsized.url;
+    console.log(res.data);
+    makeImg(gif);
 }
 
 function makeImg(url) {
-    console.log('this is url', url);
     const imgDiv = document.createElement('div');
     const img = document.createElement('img');
     img.setAttribute('src', url);
     container.append(imgDiv);
     imgDiv.append(img);
+    imgDiv.classList.add('imgDiv');
+    img.classList.add('img');
 }
 
 
