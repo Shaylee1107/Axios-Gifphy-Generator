9a7742c1a98a579a7cff12d47686addbd0bb8137
Shaylee <shayleebaumgartner1107@gmail.com>
2024-02-20 19:10:36 -0500

    adding login feature and img function

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 console.log("Let's get this party started!");
 
-async function getInfo(){
-    const form = document.querySelector('#form');
-    const input = document.querySelector('#search');
+async function logIn(){
+    const res = await axios.post('http://upload.giphy.com/v1/gifs', { 
+        param : 
+        {api_key: "nXxcwEd9rDgAdXDeVZ3zlITkdvkSOOi5", 
+        username: "ShayleeB", 
+        source_image_url: "http://www.mysite.com/myfile.mp4"
+    }});
+    return res; 
+}
 
-    const res = await axios.get('http://api.giphy.com/v1/gifs/search?q=dog&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym');
+async function getGif(search){
+    logIn();
+    const res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
     console.log(res.data);
     const firstGif = res.data.data[0].url;
     console.log('this is first gif', firstGif);
-
-
+    makeImg(firstGif);
 }
 
-getInfo()
+function getInput(){
+    const input = document.querySelector('#search');
+    console.log(input.value);
+    getGif(input.value);
+}
+
+function makeImg(url) {
+    console.log('this is url', url);
+    const container = document.querySelector('#container');
+    const imgDiv = document.createElement('div');
+    const img = document.createElement('img');
+    img.setAttribute('src', url);
+    container.append(imgDiv);
+    imgDiv.append(img);
+}
+
+// getInfo()
