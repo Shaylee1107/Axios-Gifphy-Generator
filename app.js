6cc9c1df99107b55b9c54ca794a5110c1c19ff51
Shaylee <shayleebaumgartner1107@gmail.com>
2024-02-21 10:51:33 -0500

    cleaning up code

--- app.js
+++ app.js
@@ -1,34 +1,22 @@
 console.log("Let's get this party started!");
 
-async function logIn(url){
-    console.log('logIn start')
-    // const res = await axios.post('http://upload.giphy.com/v1/gifs', { 
-    //     param : 
-    //     {api_key: "nXxcwEd9rDgAdXDeVZ3zlITkdvkSOOi5", 
-    //     username: "ShayleeB", 
-    //     source_image_url: url
-    // }});
-    const xhr = $.post("http://upload.giphy.com/v1/gifs", {api_key: "nXxcwEd9rDgAdXDeVZ3zlITkdvkSOOi5", username: "ShayleeB", source_image_url: url});
-    xhr.done(function(data) { console.log("success got data", data); });
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', getInput);
 
-    // var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
-    // xhr.done(function(data) { console.log("success got data", data); });
-    // console.log('logIn() res value', res);
-    makeImg(url);
-}
-
-async function getUrl(search){
-    const res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
-    console.log('getUrl() res.data', res.data);
-    const firstGif = res.data.data[0].images.downsized.url;
-    console.log('getUrl(): this is first gif', firstGif);
-    logIn(firstGif);
-}
-
-function getInput(){
+function getInput(e){
+    e.preventDefault();
     const input = document.querySelector('#search');
     console.log('getInput() this is input value', input.value);
     getUrl(input.value);
+    input.value = '';
+}
+
+async function getUrl(search){
+    const res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=nXxcwEd9rDgAdXDeVZ3zlITkdvkSOOi5`);
+    console.log('getUrl() res.data', res.data);
+    const firstGif = res.data.data[0].images.downsized.url;
+    console.log('getUrl(): this is first gif', firstGif);
+    makeImg(firstGif);
 }
 
 function makeImg(url) {
@@ -41,4 +29,4 @@ function makeImg(url) {
     imgDiv.append(img);
 }
 
-// getInfo()
+
